512efdd821a03c14daa0ad48b1fe476022a5ca4c
danielw4335 <daniel.wallache@gmail.com>
2025-04-02 12:24:04 +0300

    Add txt to img

--- js/controlers/meme-controler.js
+++ js/controlers/meme-controler.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var gElCanvas
 var gCtx
 
@@ -16,7 +18,24 @@ function renderMeme() {
     img.onload = () => {
         gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        renderText()
     }
-    renderText()
 }
 
+   
+function renderText() {
+    let x = 190
+    let y = 150
+    // let line = gMeme.lines[0]
+    // console.log(line)
+    // console.log(gCtx)
+    // console.log( typeof line.size )
+    gCtx.lineWidth = 0.1
+    gCtx.strokeStyle = 'brown'
+    gCtx.fillStyle = 'black'
+    gCtx.font = '40px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+    gCtx.fillText('text', x, y)
+    gCtx.strokeText('text', x, y)
+}
